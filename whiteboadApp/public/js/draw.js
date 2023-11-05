f5be35d842d9442ff0618c3286e605edaf86c690
e204239 <e204239nd@kurume-it.ac.jp>
2023-11-05 10:50:24 +0900

    描画機能をwhiteboadAppへ統合

--- whiteboadApp/public/js/draw.js
+++ whiteboadApp/public/js/draw.js
@@ -1,23 +1,30 @@
+const svg = document.getElementById("svg");
+
 let count = 0;
-const mySvg = document.getElementById("svg");
-const textBox_menu = document.getElementById("textBox_menu");
-const imgBox_menu = document.getElementById("imgBox_menu");
-const updateBtn = document.getElementById("update");
+//描画モード切り替え用
+let drawMode = "";
 
-//テキストボックスメニューがドラッグされているとき
-//  draggableで要素のドラッグを可能にしないとdragイベントが使えない
-// textBox_menu.addEventListener("dragend", displayToTextbox);
-imgBox_menu.addEventListener("dragend", displayToImgbox);
+document.addEventListener("DOMContentLoaded", () => {
+  //テキストボックスメニューがドラッグされているとき
 
+  const imgBox_menu = document.getElementById("imgBox_menu");
+  const textBox_menu = document.getElementById("textBox_menu");
+  const arrow_menu = document.getElementById("arrow_menu");
+  imgBox_menu.addEventListener("dragstart", () => (drawMode = "imgbox"));
+  imgBox_menu.addEventListener("dragend", displayToImgbox);
+  textBox_menu.addEventListener("dragstart", () => (drawMode = "textbox"));
+  textBox_menu.addEventListener("dragend", displayToTextbox);
+  arrow_menu.addEventListener("click", () => (drawMode = "arrow"));
+  arrow();
+});
 
 //テキストボックスを表示する
 function displayToTextbox(e) {
-  console.log(e);
   const foreignObject = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "foreignObject"
   );
-  const r = mySvg.getBoundingClientRect();
+  const r = svg.getBoundingClientRect();
   const x = Math.round(e.clientX - r.left);
   const y = Math.round(e.clientY - r.top);
   const textarea = document.createElement("textarea");
@@ -30,22 +37,21 @@ function displayToTextbox(e) {
   });
   textarea.setAttribute("placeholder", "文字を入力してください");
   foreignObject.appendChild(textarea);
-  mySvg.appendChild(foreignObject);
+  svg.appendChild(foreignObject);
   count++;
 }
 
 //画像を挿入するためのボックスを表示する
 function displayToImgbox(e) {
+  
   const foreignObject = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "foreignObject"
   );
   const div = document.createElement("div");
-  const r = mySvg.getBoundingClientRect();
+  const r = svg.getBoundingClientRect();
   const x = Math.round(e.clientX - r.left);
   const y = Math.round(e.clientY - r.top);
-  const form = document.createElement("form");
-  const input = document.createElement("input");
   div.innerHTML = `<form id="form${count}">
   <input id="input${count}" type="file" style="margin-bottom:15px;  accept="image/*"></input>
   <input type="submit"></input>
@@ -60,7 +66,7 @@ function displayToImgbox(e) {
     height: 100,
   });
   foreignObject.appendChild(div);
-  mySvg.appendChild(foreignObject);
+  svg.appendChild(foreignObject);
   //画像ファイルをアップロードしたときの処理
   const formRef = document.querySelector(`#form${count}`);
   const inputRef = document.querySelector(`#input${count}`);
@@ -92,8 +98,8 @@ function displayToImgbox(e) {
       x: x,
       y: y,
     });
-    mySvg.removeChild(foreignObject);
-    mySvg.appendChild(image);
+    svg.removeChild(foreignObject);
+    svg.appendChild(image);
     count++;
   };
   formRef.addEventListener("submit", submitHundler);
@@ -106,5 +112,67 @@ function setAttributes(element, attributes) {
   const arr = Object.entries(attributes);
   arr.forEach(function ([attribute, value]) {
     element.setAttribute(attribute, value);
+  });
+}
+
+let tmpX, tmpY;
+function arrow() {
+  const svg = document.querySelector("#svg");
+  let segs = [];
+  let px, py;
+  let arrow_endX, arrow_endY;
+  svg.addEventListener("mousedown", (e) => {
+    if (drawMode != "arrow") {
+      return false;
+    }
+    const clientRect = e.target.getBoundingClientRect();
+    // ページの左端から、要素の左端までの距離
+    px = e.offsetX;
+    // ページの上端から、要素の上端までの距離
+    py = e.offsetY;
+
+    const paper = Snap("#svg");
+    arrow_endX = px + 1;
+    arrow_endY = py;
+    segs = [
+      ["M", px, py],
+      ["L", arrow_endX, arrow_endY],
+    ];
+
+    //   debugFunc("x:" + px + " y:" + py);
+    //矢印の描画
+
+    let moveFlag = false;
+    let arrow;
+    let tmpX, tmpY;
+    // マウスダウンイベント
+    paper.mousedown(function (e) {
+      if (drawMode != "arrow") {
+      return false;
+    }
+      moveFlag = true;
+      tmpX = e.offsetX;
+      tmpY = e.offsetY;
+      arrow = paper.path("M " + e.offsetX + " " + e.offsetY);
+      paper.attr({ stroke: "black", strokeWeight: 1, id: "arrow" });
+      // debugFunc("マウスダウン");
+    });
+
+    // マウスムーブイベント
+    paper.mousemove(function (e) {
+      if (moveFlag) {
+        var d = "M " + tmpX + " " + tmpY + " L " + e.offsetX + " " + e.offsetY;
+        arrow.attr({ d: d });
+        const arrowElem = document.querySelector("#arrow");
+        arrowElem.setAttribute("marker-end", "url(#m_atr)");
+      }
+      // debugFunc("マウスムーブ");
+    });
+
+    // マウスアップイベント
+    paper.mouseup(function () {
+      moveFlag = false;
+      // debugFunc("マウスアップ");
+    });
   });
 }
